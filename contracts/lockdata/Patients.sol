7dc7d13e0bd620f8d518c7a86cbab4abdb671e17
Nguyễn Danh Hoàng <hoangnd@vmodev.com>
2022-11-02 13:46:03 +0700

    Update Patients contract

---
+++ contracts/lockdata/Patients.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../utils/ERC721Base.sol";
+import "./DDR.sol";
+import "../interface/IDDR.sol";
+
+
+contract Patients is ERC721Base{
+  mapping(uint256 => bytes32) private _PatientsHash;
+  mapping(uint256 => uint256[]) private _DDROfPatients;
+  address private DDRAddress;
+  // Prescription private _prescription;
+  mapping(uint256 => bytes32) private _DDRHash;
+
+  constructor(address _DDRAddress, address _authAddress)
+    ERC721Base("Patients", "PT", _authAddress)
+  {
+    // _prescription = Prescription(prescription);
+    DDRAddress = _DDRAddress;
+  }
+
+  function mint(bytes32 hashValue, string memory uri, string memory data, uint256[] memory listId) public returns(uint256){
+      require(keccak256(abi.encodePacked(data)) == hashValue, "Data Integrity fail");
+      uint256 tokenId = super.mint(uri);
+      _PatientsHash[tokenId] = hashValue;
+      _DDROfPatients[tokenId] = listId;
+      IDDR(DDRAddress).setLockDDR(listId, msg.sender);
+    return tokenId;
+  }
+
+  // function hashVerify(bytes32 hashValue, )
+
+  function discloseApproval(address _authAddress, uint256 tokenId) public virtual {
+    address owner = ERC721.ownerOf(tokenId);
+    require(_authAddress != owner, "ERC721: approval to current owner");
+    // _prescription.discloseApproval(tokenId, _authAddress);
+    // IPrescription(prescriptionAddress).discloseApproval(tokenId, _authAddress);
+  } 
+
+
+
+  function checkDataIntegrity(uint256 PatientsId, bytes32 hashValue)
+        public
+        view
+        returns (bool, uint256[] memory)
+    {
+        return (
+            _PatientsHash[PatientsId] == hashValue,
+            _DDROfPatients[PatientsId]
+        );
+    }
+  function getHashValueFromDDR(uint256 tokenId) public view returns(bytes32){
+      return IDDR(DDRAddress).getHashValue(tokenId);
+
+  }
+}
+
