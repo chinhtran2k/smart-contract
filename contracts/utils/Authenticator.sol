4769a7297acfcd75656b79305255fd6bf06bcef8
Nguyễn Danh Hoàng <hoangnd@vmodev.com>
2022-11-03 15:52:33 +0700

    Update enum and add clinic role

--- contracts/utils/Authenticator.sol
+++ contracts/utils/Authenticator.sol
@@ -7,6 +7,7 @@ contract Authenticator is IAuthenticator {
     // mapping(address => bool) private _administrator;
     // mapping(address => bool) private _actor;
     mapping(address => bool) private _patients;
+    mapping(address => bool) private _clinic;
 
     constructor() {
         // _administrator[msg.sender] = true;
@@ -17,20 +18,14 @@ contract Authenticator is IAuthenticator {
         require(_address != address(0), "Address zero is not allowed");
         require(
             _patients[msg.sender] == true,
-            "Address is not healthRecord"
+            "Address is not Patients"
         );
-        // if checkClaim(address A, 1) => patient
-        // else if checkClaim(address A, 2) => Pharmacy
-        // if (_subject[_address] && authType != AuthType.SB)
-        //     _subject[_address] = false;
-        if (_patients[_address] && authType != AuthType.PT)
+        if (_patients[_address] && authType != AuthType.Patients)
             _patients[_address] = false;
-        // if (_actor[_address] && authType != AuthType.IV)
-        //     _actor[_address] = false;
-
-        // if (authType == AuthType.SB) _subject[_address] = true;
-        else if (authType == AuthType.PT) _patients[_address] = true;
-        // else if (authType == AuthType.IV) _actor[_address] = true;
+        if (_clinic[_address] && authType != AuthType.Clinic)
+            _clinic[_address] = false;
+        else if (authType == AuthType.Patients) _patients[_address] = true;
+        else if (authType == AuthType.Clinic) _clinic[_address] = true;
     }
 
     function checkAuth(address _address)
@@ -40,10 +35,8 @@ contract Authenticator is IAuthenticator {
         returns (AuthType)
     {
         require(_address != address(0), "Address zero is not allowed");
-        if(_patients[_address]) return AuthType.PT;
-        // if (_subject[_address]) return AuthType.SB;
-        // else if (_administrator[_address]) return AuthType.AD;
-        // else if (_actor[_address]) return AuthType.IV;
+        if(_patients[_address]) return AuthType.Patients;
+        if(_clinic[_address]) return AuthType.Clinic;
         else return AuthType.NONE;
     }
 
@@ -57,13 +50,13 @@ contract AuthenticatorHelper {
         _IAuth = IAuthenticator(_authenticator);
     }
 
-    // modifier onlyClinic() {
-    //     require(
-    //         _IAuth.checkAuth(msg.sender) == AuthType.CL,
-    //         "Only actor_clinic can call this function"
-    //     );
-    //     _;
-    // }
+    modifier onlyClinic() {
+        require(
+            _IAuth.checkAuth(msg.sender) == AuthType.Clinic,
+            "Only actor_clinic can call this function"
+        );
+        _;
+    }
 
     modifier onlyPatients() {
         require(
