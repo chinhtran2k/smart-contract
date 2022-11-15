b7c339f38a734e8355cf913965ad2d162f193b3b
thanh <trongthanht3@gmail.com>
2022-11-15 09:12:50 +0700

    fix missing DID

---
+++ contracts/DID/ERC735.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+abstract contract ERC735 {
+
+    event ClaimRequested(uint256 indexed claimRequestId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);    event ClaimAdded(bytes32 indexed claimId, uint256 indexed claimType, address indexed issuer, uint256 signatureType, bytes32 signature, bytes claim, string uri);
+    event ClaimAdded(bytes32 indexed claimId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
+    event ClaimRemoved(bytes32 indexed claimId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
+    event ClaimChanged(bytes32 indexed claimId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
+
+    struct Claim {
+        uint256 claimType;
+        uint256 scheme;
+        address issuer; // msg.sender
+        bytes signature; // this.address + claimType + data
+        bytes data;
+        string uri;
+    }
+
+    function getClaim(bytes32 _claimId) public virtual returns(uint256 claimType, uint256 scheme, address issuer, bytes memory signature, bytes memory data, string memory uri);
+    function getClaimIdsByType(uint256 _claimType) public virtual returns(bytes32[] memory claimIds);
+    function addClaim(uint256 _claimType, uint256 _scheme, address issuer, bytes memory _signature, bytes memory _data, string memory _uri) public virtual returns (bytes32 claimRequestId);
+    function removeClaim(bytes32 _claimId) public virtual returns (bool success);
+}
